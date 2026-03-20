1704638cecd58612d9d3efe5174175f42683b43c
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-03-20 15:33:21 +0100

    create Footer

--- src/Components/AppFooter.jsx
+++ src/Components/AppFooter.jsx
@@ -1,12 +1,15 @@
+import menu from "../Data/menu";
+import { Link } from "react-router-dom";
 
 export default function AppFooter() {
 
     return (
         <footer>
-            <div >
-                <h1>Footer</h1>
+            <div className="text-center text-light">
+                <h1>Pronto a fare Shopping?</h1>
+                <p>Oltre 200 prodotti disponibili, aggiornati in tempo reale.</p>
 
-
+                <Link to={menu[2].path} className="nav-footer">Vai al prodotti</Link>
             </div>
         </footer>
     )
